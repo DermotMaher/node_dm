35ffb64b16e08dd7ea6e17506bd80f30894c690f
Dermot Maher <dermotowenolivermaher@gmail.com>
2019-02-07 10:13:01 +0000

    commented out log

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ app.get('/', (req,res)=>{
   res.sendFile(__dirname + '/index.html');
 });
 app.listen (3014);
-console.log("Server listening on port 3014");
+//console.log("Server listening on port 3014");
